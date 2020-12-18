b313e1ea2c0abaf3d324fca57c310804372bb4c3
Ehsan Fani <efani@benhur.de>
2020-12-18 17:25:10 +0100

    commit and add

--- eproject/eproj/eprojapp/noise_sensor.py
+++ eproject/eproj/eprojapp/noise_sensor.py
@@ -3,26 +3,6 @@ import pyaudio
 import struct
 import datetime
 import alsaaudio
-import matplotlib.pyplot as plt
-import numpy as np
-
-##--------------------------Start of ploting--------------------------------#
-i=0
-f,ax = plt.subplots(2)
-x = np.arange(10000)
-y = np.random.randn(10000)
-li, = ax[0].plot(x, y)
-ax[0].set_xlim(0,1000)
-ax[0].set_ylim(-5000,5000)
-ax[0].set_title("Raw Audio Signal")
-li2, = ax[1].plot(x, y)
-ax[1].set_xlim(0,5000)
-ax[1].set_ylim(-100,100)
-ax[1].set_title("Fast Fourier Transform")
-plt.pause(0.01)
-plt.tight_layout()
-##--------------------------End of ploting--------------------------------#
-
 
 FORMAT = pyaudio.paInt16  # We use 16bit format per sample
 CHANNELS = 1
@@ -40,7 +20,6 @@ def plot_data(in_data):
     global MUTE_LOCKED
     global VOLUME
     global MIXER
-    # get and convert the data to float
     audio_data = struct.unpack("h", in_data[:2])
     ct = datetime.datetime.now()
     if audio_data[0] > 18000:
@@ -54,32 +33,21 @@ def plot_data(in_data):
             MIXER.setvolume(100)  # Set the volume to 100%.
             VOLUME = 100
             print("unmute_command")
+
     print("current time:-", ct)
     print("volume:", VOLUME)
-
-#---------------------------------Start of Rendering-------------------------------------
-    audio_data = np.fromstring(in_data, np.int16)
-    scale_variable = 10.*np.log10(abs(np.fft.rfft(audio_data)))
-    li.set_xdata(np.arange(len(audio_data)))
-    li.set_ydata(audio_data)
-    li2.set_xdata(np.arange(len(scale_variable))*10.)
-    li2.set_ydata(scale_variable)
-    plt.pause(0.01)
-#---------------------------------End of Rendering-------------------------------------
 
 def main():
     logging.basicConfig(filename='logFile.log', level=logging.DEBUG)
     audio = pyaudio.PyAudio()
+
     stream = audio.open(format=FORMAT,
                         channels=CHANNELS,
                         rate=RATE,
                         input=True)  # ,
     stream.start_stream()
-    print("\n+---------------------------------+")
-    print("| Press Ctrl+C to Break Recording |")
-    print("+---------------------------------+\n")
-
     keep_going = True
+
     while keep_going:
         try:
             plot_data(stream.read(CHUNK, exception_on_overflow=False))
