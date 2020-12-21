bad1ae80b575f297aba4aa42fa2fbf2dfd025e3a
Ehsan Fani <efani@benhur.de>
2020-12-21 14:04:13 +0100

    adding toggle btn

--- eproject/eproj/eprojapp/views.py
+++ eproject/eproj/eprojapp/views.py
@@ -2,10 +2,9 @@ from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, redirect
 from django.views.generic import CreateView
-
-from eprojapp import forms
+from eprojapp import forms, noise_sensor
 from eprojapp.models import User, Tokens, Car
-
+import sys
 
 # Create your views here.
 
@@ -58,6 +57,23 @@ class CarCreateView(CreateView):
     model = Car
     fields = ('model', 'series', 'kilometer', 'price', 'img')
 
+#----------printong error in browser------add later
+
+# def post(self, request, format=None):
+#     serializer = DataSerializer(data=request.data)
+#     if serializer.is_valid():
+#         serializer.save()
+#         return Response(serializer.data, status=status.HTTP_201_CREATED)
+#     else:
+#         error = Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#         print(error.data)
+#     return error
 
 def sound_sensor(request):
+    MAX_RANGE=60000
+    MIN_RANGE=0
+    #noise_sensor.main()
+    #post()
+    #sys.exit()
+
     return render(request, 'sound_sensor.html')
